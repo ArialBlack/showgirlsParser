b998c0c0d402e126c2c0332b2e733c6db3799bf5
ArialBlack <andreyturik@gmail.com>
2015-12-20 20:27:26 +0200

    init

---
+++ js/getPagesSource.js
@@ -0,0 +1,44 @@
+// @author Rob W <http://stackoverflow.com/users/938089/rob-w>
+// Demo: var serialized_html = DOMtoString(document);
+
+function DOMtoString(document_root) {
+    var html = '',
+        node = document_root.firstChild;
+    while (node) {
+        switch (node.nodeType) {
+        case Node.ELEMENT_NODE:
+            html += node.outerHTML;
+            break;
+        case Node.TEXT_NODE:
+            html += node.nodeValue;
+            break;
+        case Node.CDATA_SECTION_NODE:
+            html += '<![CDATA[' + node.nodeValue + ']]>';
+            break;
+        case Node.COMMENT_NODE:
+            html += '<!--' + node.nodeValue + '-->';
+            break;
+        case Node.DOCUMENT_TYPE_NODE:
+            // (X)HTML documents are identified by public identifiers
+            html += "<!DOCTYPE " + node.name + (node.publicId ? ' PUBLIC "' + node.publicId + '"' : '') + (!node.publicId && node.systemId ? ' SYSTEM' : '') + (node.systemId ? ' "' + node.systemId + '"' : '') + '>\n';
+            break;
+        }
+        node = node.nextSibling;
+    }
+    
+    //return html;
+    var images = [].slice.apply(document.getElementsByTagName('img'));
+    var imageURLs = images.map(function(image) {
+        if( image.clientWidth > 639 && image.clientHeight > 479) {
+            return image.src;
+        }
+    });
+    
+    return images;
+
+}
+
+chrome.runtime.sendMessage({
+    action: "getSource",
+    source: DOMtoString(document)
+});
